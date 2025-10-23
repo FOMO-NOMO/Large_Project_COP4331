19cc073a3216d60ca632bec69aaaae6bafbb9c8c
Caleb Yaghoubi <Caleby315@gmail.com>
2025-10-23 03:51:20 +0000

    Added mern C files

--- main/server.js
+++ main/server.js
@@ -108,90 +108,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use((req, res, next) => 
-{
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, DELETE, OPTIONS'
-  );
-  next();
-});
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+const url = process.env.mongoUri;
+const client = new MongoClient(url);
+client.connect();
 
-// API endpoints
-app.post('/api/addcard', async (req, res, next) =>
-{
-  // incoming: userId, color
-  // outgoing: error
-
-  var error = '';
-
-  const { userId, card } = req.body;
-
-  // TEMP FOR LOCAL TESTING.
-  cardList.push( card );
-   var ret = { error: error };
-  res.status(200).json(ret);
-});
-
-app.post('/api/login', async (req, res, next) => 
-{
-  // incoming: login, password
-  // outgoing: id, firstName, lastName, error
-
-  var error = '';
-
-  const { login, password } = req.body || {};
-
-  var id = -1;
-  var fn = '';
-  var ln = '';
-
-  // guard against undefined login
-  const loginLower = (typeof login === 'string') ? login.toLowerCase() : '';
-
-  if( loginLower === 'rickl' && password === 'COP4331' )
-  {
-    id = 1;
-    fn = 'Rick';
-    ln = 'Leinecker';
-  }
-  else
-  {
-    error = 'Invalid user name/password';
-  }
-
-  var ret = { id:id, firstName:fn, lastName:ln, error:error};
-  res.status(200).json(ret);
-});
-
-app.post('/api/searchcards', async (req, res, next) => 
-{
-  // incoming: userId, search
-  // outgoing: results[], error
-
-  var error = '';
-
-  const { userId, search } = req.body;
-  var _search = search.toLowerCase().trim();
-  var _ret = [];
-
-  for( var i=0; i<cardList.length; i++ )
-  {
-    var lowerFromList = cardList[i].toLocaleLowerCase();
-    if( lowerFromList.indexOf( _search ) >= 0 )
-    {
-      _ret.push( cardList[i] );
-    }
-  }
-
-  var ret = {results:_ret, error:''};
-  res.status(200).json(ret);
-});
+var api = require('./api.js');
+api.setApp( app, client );
 
 app.listen(5000); // start Node + Express server on port 5000
 console.log('Server started on port 5000');
