9c9af5d3c700b150ea52154a9b880e52e07c72c4
Caleb Yaghoubi <Caleby315@gmail.com>
2025-10-23 04:07:19 +0000

    Fixed path.js

--- main/frontend/src/components/Path.js
+++ main/frontend/src/components/Path.js
@@ -1,9 +1,10 @@
-const app_name = 'fomonomo.xyz'
-exports.buildPath = function buildPath(route: string): string {
-    if (process.env.NODE_ENV != 'development') {
+
+const app_name = 'fomonomo.xyz';
+
+export function buildPath(route) {
+    if (process.env.NODE_ENV !== 'development') {
         return 'http://' + app_name + ':5000/' + route;
-    }
-    else {
+    } else {
         return 'http://localhost:5000/' + route;
     }
 }
