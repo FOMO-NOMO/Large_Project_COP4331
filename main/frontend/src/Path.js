df8a2bf42078a8865079fa98fc3972d2a9c9faac
collin vanmeter <metervancollin@gmail.com>
2025-10-30 17:39:28 -0400

    edit localhost 5000 back to norm (my mac needs 5001

--- main/frontend/src/Path.js
+++ main/frontend/src/Path.js
@@ -6,7 +6,7 @@ const app_name = 'fomonomo.xyz';
 export function buildPath(route) {
   const baseUrl =
     process.env.NODE_ENV === 'development'
-      ? 'http://localhost:5001/'       // local backend
+      ? 'http://localhost:5000/'       // local backend
       : `http://${app_name}:5000/`;    // production backend (HTTP only for now)
 
   return baseUrl + route;
