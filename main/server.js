4c00c7228ba2ba8523f66c95a131fc6602e4a415
collin vanmeter <metervancollin@gmail.com>
2025-10-30 16:55:02 -0400

    configured cors to allow deployed domain and api req

--- main/server.js
+++ main/server.js
@@ -2,8 +2,22 @@
 const express = require('express');
 const cors = require('cors');
 
+
+// cors config to allow requests from frontend
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://fomonomo.xyz',
+    'http://fomonomo.xyz' // we only have http for now
+  ];
+
+app.use(cors({
+    origin: allowedOrigins,
+    credentials: true, // if you use cookies/JWT
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+  }));
+
 const app = express();
-app.use(cors());
 app.use(express.json());
 
 const MongoClient = require('mongodb').MongoClient;
@@ -15,5 +29,7 @@ client.connect();
 var api = require('./api.js');
 api.setApp( app, client );
 
-app.listen(5000); // start Node + Express server on port 5000
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT); // start Node + Express server on port 5000
 console.log('Server started on port 5000');
