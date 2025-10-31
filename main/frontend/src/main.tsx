1a4fae3d793849dfa28ea5fd5e39561132ff72ed
Ameer Khalil <ameerkhalil795@gmail.com>
2025-10-31 11:17:13 -0400

    Fix minor issues with git pull

--- main/frontend/src/main.tsx
+++ main/frontend/src/main.tsx
@@ -1,12 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-<<<<<<< HEAD
+
 import './styles/main.css';
 import App from './App.tsx'
-=======
-import './index.css'
-import App from './App'
->>>>>>> 294629c6e2c92cc3ea407a75375a0427dae413c3
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
